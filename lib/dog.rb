dc33a34f9d2dd372b88a88df842016e8180329ad
Zietieflr <lrmcguire93@gmail.com>
2020-05-15 10:05:26 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,4 @@
 class Dog
-  # attr_accessor :name 
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
@@ -10,6 +9,6 @@ class Dog
 end
 
 lassie = Dog.new
-lassie.name = "Lassie"
+lassie.name = 'Lassie'
 
 puts lassie.name
